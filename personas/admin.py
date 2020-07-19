6b5d1c04db388834fbd1f3bfbdca6464e3595787
Linda Patarroyo <linda.patarroyo@unillanos.edu.co>
2020-07-18 22:16:10 -0500

    Modelo de cliente

--- personas/admin.py
+++ personas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from personas.models import Client
+
+
+@admin.register(Client)
+class AdminClient(admin.ModelAdmin):
+    pass
--- personas/models.py
+++ personas/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Client (models.Model):
+    phone = models.BigIntegerField()
+
+    address = models.CharField(max_length=255)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    class Meta:
+        verbose_name="Cliente"
+        verbose_name_plural = "Clientes"
+    def __str__(self):
+        return self.user.username
