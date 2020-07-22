6b3653a49681b8864fb71386017118131a7ae68a
laura <laespaviza@gmail.com>
2020-07-21 20:36:41 -0500

    modifique modelo productos

--- productos/models.py
+++ productos/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from empresas.models import Shop
+
+
 class Category (models.Model):
     name = models.CharField(max_length=50)
     icon = models.ImageField(upload_to = None, height_field = None, width_field = None, max_length = 100)
@@ -12,7 +15,7 @@ class Category (models.Model):
         verbose_name="Categoria"
         verbose_name_plural = "Categorias"
     def __str__(self):
-        return self.user.username
+        return self.name
 
 class Unit (models.Model):
     name = models.CharField(max_length=50)
@@ -21,7 +24,7 @@ class Unit (models.Model):
         verbose_name="Unidad"
         verbose_name_plural = "Unidades"
     def __str__(self):
-        return self.user.username
+        return self.name
 
 
 class Product (models.Model):
@@ -31,12 +34,12 @@ class Product (models.Model):
     valor = models.IntegerField()
     quantity = models.IntegerField()
     discount = models.IntegerField()
-    # category = models.ManyToManyField(Category)
-    # unidades = models.ManyToManyField(Unit)
-    # shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    unidades = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name="Producto"
         verbose_name_plural = "Productos"
     def __str__(self):
-        return self.user.username
+        return self.name
