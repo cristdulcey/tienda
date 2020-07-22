c04ab187a1edf363cc281045b43959bb8f344709
Linda Patarroyo <linda.patarroyo@unillanos.edu.co>
2020-07-21 20:25:05 -0500

    Arreglar personas

--- personas/models.py
+++ personas/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 
 # Create your models here.
+from empresas.models import Shop
+
+
 class Client (models.Model):
     phone = models.BigIntegerField()
     address = models.CharField(max_length=255)
@@ -13,23 +16,14 @@ class Client (models.Model):
         verbose_name_plural = "Clientes"
     def __str__(self):
         return self.user.username
-class User (models.Model):
-    username = models.CharField(max_length=255)
-    firts_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    class Meta:
-        verbose_name="Usuario"
-        verbose_name_plural = "Usuarios"
-    def __str__(self):
-        return self.user.username
+
 class Staff (models.Model):
     phone = models.BigIntegerField()
-    user = models.(User, on_delete=models.CASCADE)
-    id_shop = models.(Shop , on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop , on_delete=models.CASCADE)
     type = models.CharField(max_length=100)
     send_notification = models.CharField(max_length=255)
     class Meta:
         verbose_name = "Personal"
     def __str__(self):
-        return self.Staff.type
+        return self.type
